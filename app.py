f2c4ae751d8aea23a68e74f6711f8a789e188bfa
omartel-crypto <omartel@elpedregalsa.com>
2026-04-23 07:02:21 -0500

    Cambio date

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
-from datetime import datetime
+from datetime import datetime, timedelta
 import requests
 
 # --- CONFIGURACIÓN DE PÁGINA ---
